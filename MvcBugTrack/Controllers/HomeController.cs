0f4c580ae94823a0c3fc0d6b96be5aea65a31451
jesse.barocio@gmail.com <jesse.barocio@gmail.com@e589a550-e3a5-bde2-bfc6-3e55b6d4388d>
2012-02-14 18:16:38 +0000

    Forgot to add them to the repository, but here are the files I meant to commit last revision.

---
+++ MvcBugTrack/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBugTrack.Models;
+
+namespace MvcBugTrack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BugTrackContext _db = new BugTrackContext();
+        
+        
+
+        // Index action
+        public ActionResult Index()
+        {
+            var model = _db.Bugs.ToList();
+            return View(model);
+        }
+
+        
+
+        // Create actions
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Bug newBug)
+        {
+            try
+            {
+                _db.Bugs.Add(newBug);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                // TODO Add error display logic here eventually.
+                return View();
+            }
+        }
+
+
+
+        // Edit actions
+        public ActionResult Edit(int id)
+        {
+            return View(_db.Bugs.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Bug bug)
+        {
+            try
+            {
+                _db.Entry(bug).State = System.Data.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                // TODO Add error display logic here eventually.
+                return View();
+            }
+        }
+
+
+
+        // Delete actions
+        public ActionResult Delete(int id)
+        {
+            return View(_db.Bugs.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Bug bug)
+        {
+            try
+            {
+                _db.Entry(bug).State = System.Data.EntityState.Deleted;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                // TODO Add error display logic here eventually.
+                return View();
+            }
+        }
+
+
+
+        // Details actions
+        public ActionResult Details(int id)
+        {
+            return View(_db.Bugs.Find(id));
+        }
+    }
+}
---
+++ MvcBugTrack/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBugTrack.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
